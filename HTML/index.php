f7614c8ae647ca5de9f4cb300c1866cbd62c30df
Kurt2006 <35888370+Kurt2006@users.noreply.github.com>
2018-01-28 10:03:26 +0100

    First website upload
    hi

---
+++ HTML/index.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<title>JOINCRAFT</title>		
+		<link rel="stylesheet" type="text/css" href="style.css" />
+		
+		<script type='text/javascript' src='http://ajax.googleapis.com/ajax/libs/jquery/1.5.2/jquery.min.js'></script>
+		<script type='text/javascript' src='javascripts/jquery.tipsy.js'></script>
+		<script type='text/javascript'>
+		$(function() {
+			$('#tipsy').tipsy({fade: true, gravity: 's'});
+		});
+		</script>
+		<!--[if IE]>
+		<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+		<![endif]-->
+		
+	</head>
+	<body>
+		<div class="wrapper">
+			<img src="images/logo.png" title="Akardoo Network" style="width:20%; height:20%;"/>
+<br>
+</br>
+<br>
+</br>
+			<img src="images/construccion.png" align="left" style="width:38%; height:70%;"/>
+			<h1>Maintenance!</h1>
+
+			<p>Entschuldigung, wir arbeiten grade am Shop! <strong>Es kann sein das der Shop schon verfügbar ist! Bitte schaue in die Updates!</strong></p>
+</br>
+</br>
+</br>
+<br>
+						
+			<section class="progress">
+				<div class="progress-bar-container" id="tipsy" title="35% Abgeschlossen"> <!-- Edit this title for the tooltip pop up -->
+					<article class="progress-bar" style="width:35%"  ></article> <!-- Edit the width percentage value to indicate progress -->
+				</div>
+
+
+<!------------------- Article-------->
+			</section>
+<div class="clear"></div>		</div>
+<div class="footer">
+	<div class="footer-container">
+    	
+    	<ul class="footer-menu">
+            <li class="footer-menu-title">Nützliche links</li>
+            <li><a href="#">In arbeit!</a></li>
+            <li><a href="#">In arbeit!</a></li>
+            <li><a href="#">In arbeit!</a></li>
+        </ul>        
+        
+    	<div class="footer-dummy"></div>
+        
+      	<div class="footer-data">
+        	All rights reserved 2018 &copy; Joincraft<br> 
+       		<div class="footer-links">
+       			<a href="#">About us</a>  /  <a href="#">Kontakt</a>  /  <a href="#">ToS</a>
+       		</div>
+        	<a href="https://twitter.com/intent/follow?original_referer=http%3A%2F%2Fakardoo.com%2F&region=follow_link&screen_name=Akardoo&tw_p=followbutton&variant=2.0" target="_blank" class="follow-twitter">Follow @Joincraft</a>
+
+        	<a href="http://fb.akardoo.com" target="_blank" class="follow-facebook">Follow @Joincraft</a>
+
+        </div>
+    
+    </div>	</body>
+</html>
